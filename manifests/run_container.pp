327462ab998ed1621f8d85b4222ddfeec54b90b3
Mark.Fieldhouse@mafitsulting.co.uk <root@puppet.localdomain>
2017-11-20 12:07:32 +0000

    updated init

--- manifests/run_container.pp
+++ manifests/run_container.pp
@@ -1,6 +1,10 @@
-docker::run { 'mywebserver':
-  ensure => 'present',
-  image => 'fieldhousem/myapacheweb:v1',
-  ports  => '80',
-  expose => '80',
+class docker_management::run_container {
+
+  docker::run { 'mywebserver':
+    ensure => 'present',
+    image => 'fieldhousem/myapacheweb:v1',
+    ports  => '80',
+    expose => '80',
+  }
+
 }
