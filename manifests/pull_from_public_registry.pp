91793e1a6cc56c2ebe66c09df1811ab4e1efd3c6
Mark.Fieldhouse@mafitsulting.co.uk <root@puppet.localdomain>
2017-11-20 09:39:08 +0000

    updated manifest

--- manifests/pull_from_public_registry.pp
+++ manifests/pull_from_public_registry.pp
@@ -1,4 +1,10 @@
-class docker_management::pull_from_public_registry {
+class docker_management::pull_from_public_registry (
+  String $password,
+){
+
+  notify { 'password_notifcation': 
+    message => "This is a notification from ${password}",
+  }
 
   docker::registry { 'https://index.docker.io/v1/':
     username  => 'fieldhousem',
