0b414b8d0e5a6ea067ad57a98a8f3f5780fdf61d
Mark.Fieldhouse@mafitsulting.co.uk <root@puppet.localdomain>
2017-11-20 12:10:27 +0000

    updated init

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,5 +2,5 @@ class docker_management (
   String $password = $::docker_management::params::password,
 ) inherits ::docker_management::params {
   class { '::docker_management::pull_from_public_registry': }
-  -> class { '::docker_management::run_container.pp': }
+  -> class { '::docker_management::run_container': }
 }
